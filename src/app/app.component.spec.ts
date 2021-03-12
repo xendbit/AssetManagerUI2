236469f00220b4f3aed4a5d744f1ca6e2717d6b9
spockula <chinedukogu@gmail.com>
2021-03-12 10:03:48 +0100

    updated naming

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ArtExchange'`, () => {
+  it(`should have as title 'XendSwap'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ArtExchange');
+    expect(app.title).toEqual('XendSwap');
   });
 
   it('should render title', () => {
