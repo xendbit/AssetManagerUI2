dd9b75899472a4341537d1ed5830af283c674532
spockula <chinedukogu@gmail.com>
2021-05-19 17:56:34 +0100

    updated look and feel for portfolio page

--- src/app/my-portfolio/portfolio.component.ts
+++ src/app/my-portfolio/portfolio.component.ts
@@ -18,13 +18,15 @@ export class PortfolioComponent implements OnInit {
   balance: any;
   isConnected: boolean;
   account: any;
+  images: any[];
+  videos: any[];
+  audios: any[];
 
   constructor(public assetService: AssetsService, public router: Router) { }
   ngOnInit() {
-    
-     
-    console.log('leke', this.account)
-    
+    this.audios = [];
+    this.images = [];
+    this.videos = [];
     this.getAssets();
     this.getAllAssets();
   }
@@ -32,31 +34,59 @@ export class PortfolioComponent implements OnInit {
   getAssets() {
     this.assetService.showSpinner();
     this.assetService.getMetamaskInfo().then(data => {
-      this.userId = data.account;
       this.account = data.account;
       console.log('this is user', this.userId)
       // this.displayedData = data.displayedData;
       this.balance = data.balance;
-      console.log('confirmmmm', this.userId)
-      this.assetService.getAssetsByOwnerId(this.userId).subscribe(data => {
+      this.assetService.getAssetsByOwnerId(this.account).subscribe(data => {
         console.log('this is data gotten', data);
         const res = data['data']['items'];
         let initial = [];
         let finalData = [];
         res.forEach( element => {
-          if (element.media.length > 0) {
-            initial.push(element.media)
-            initial.forEach(mel => {
-              mel.forEach( data => {
-                if (data.mediaKey === 'image') {
-                if (element.media.find(elem => elem === data )){
-                  finalData.push({name: element.name, data, tokenId: element.tokenId, symbol: element.symbol, owner: element.owner, issuer: element.issuer, id: element.id, dateIssued: element.dateIssued})
-                }
-                this.myAssets = finalData;
-              }
-          })
-        })
-      }
+          if (element.media.length > 0 ) {
+            if (element.category === 'artwork') {
+              console.log('yes')
+              const image = element.media.filter(x => {
+                return x.mediaKey ==='image';
+              })[0];
+              const mp4 = element.media.filter(x => {
+                return x.mediaKey ==='mp4';
+              })[0];
+              this.images.push({name: element.name, image: image, video: mp4, tokenId: element.tokenId, symbol: element.symbol, owner: element.owner, issuer: element.issuer, id: element.id, dateIssued: element.dateIssued})
+            } else {
+              console.log('there is none')
+            }
+            console.log('this is images', this.images)
+           
+  
+            if(element.category === 'musicRight') {
+              console.log('yes!')
+              const mp3 = element.media.filter(x => {
+                console.log('this is ex', x)
+                return x.mediaKey ==='mp3';
+              })[0];
+              const image =  element.media.filter(x => {
+                return x.mediaKey ==='image';
+              })[0]; 
+              
+              this.audios.push({name: element.name, image: image, audio: mp3, tokenId: element.tokenId, symbol: element.symbol, owner: element.owner, issuer: element.issuer, id: element.id, dateIssued: element.dateIssued})
+              console.log('this is audio', this.audios)
+            }
+            if(element.category === 'movieRight') {
+              console.log('yes!!')
+              const mp4 = element.media.filter(x => {
+                return x.mediaKey ==='mp4';
+              })[0];
+              const image =  element.media.filter(x => {
+                return x.mediaKey ==='image';
+              })[0];   
+              
+              this.videos.push({name: element.name, image:image, video: mp4, tokenId: element.tokenId, symbol: element.symbol, owner: element.owner, issuer: element.issuer, id: element.id, dateIssued: element.dateIssued})
+              console.log('this is videos', this.videos)
+            }
+          }
+          
       })
         this.assetService.stopSpinner();
         console.log('these are my assets, ', this.myAssets);
