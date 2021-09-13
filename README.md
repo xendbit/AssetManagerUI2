7c3b5a2c016ca26d938e1129864d5fde9ca88b7e
femiashiru <femi@opushive.com>
2021-09-13 16:52:00 +0100

    renamed from ArtExchange to NiftyRow and updated the version of Node in the yarn script

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ArtExchange
+# NiftyRow
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.0.1.
 
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ArtExchange'),
+      dir: require('path').join(__dirname, './coverage/NiftyRow'),
       subdir: '.',
       reporters: [
         { type: 'html' },
