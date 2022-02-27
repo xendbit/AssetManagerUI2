d46312b2b4fd156a5ae9827a5eb9ec96f4e4f1f6
spockula <chinedukogu@gmail.com>
2022-02-27 22:56:19 +0100

    updated harmony

--- src/app/components/create-assets/create-assets.component.ts
+++ src/app/components/create-assets/create-assets.component.ts
@@ -16,7 +16,7 @@ import { timeout } from 'rxjs/operators';
   styleUrls: ['./create-assets.component.scss']
 })
 export class CreateAssetsComponent implements OnInit {
- 
+
   preview: any;
   files: File[] = [];
   media: Array<ICreatorMedia> = [];
@@ -57,8 +57,8 @@ export class CreateAssetsComponent implements OnInit {
   fileSize: number;
 
   constructor( public mainService: MainService, private spinner: NgxSpinnerService, public userActions: UserActionsService,
-    public metamaskService: MetamaskService, public router: Router ) { 
-   
+    public metamaskService: MetamaskService, public router: Router ) {
+
   }
 
   ngOnInit(): void {
@@ -79,7 +79,7 @@ export class CreateAssetsComponent implements OnInit {
       //  this.spinner.hide();
      })
     }
-  
+
   }
 
   checkConnection() {
@@ -135,17 +135,17 @@ export class CreateAssetsComponent implements OnInit {
   }
 
   checkIssuer() {
-  
+
     this.mainService.checkIssuer(this.account).subscribe(res => {
       this.response = res;
     },
     error => {
       this.response = error['error'];
     })
-  
+
 }
 
-  
+
   check(file) {
     this.errorMessage = "";
     this.fileSize = file.size/1024/1024;
@@ -172,7 +172,7 @@ export class CreateAssetsComponent implements OnInit {
           this.previewArray.push({type: 'image', name: file.name, media: file})
           this.mediaType.push('image');
         }
-        if ( /\.(mp4)$/i.test(file.name) === true  ) { 
+        if ( /\.(mp4)$/i.test(file.name) === true  ) {
           // this.media.push({media: file, mediaType: 1, mediaSizeMB: fileSize});
           this.mp3 = true;
           this.previewArray.push({type: 'mp4', name: file.name, media: file})
@@ -206,13 +206,13 @@ export class CreateAssetsComponent implements OnInit {
       if (this.previewArray.filter(item => item.name !== this.preview.name)) {
         this.preview = undefined;
       }
-      
-    } 
+
+    }
   }
 
   assignPreview(asset) {
     this.preview = asset;
-   
+
   }
 
 
@@ -238,10 +238,10 @@ export class CreateAssetsComponent implements OnInit {
       this.checkConnection();
       return;
     }
-    if (this.response.data.error === 'Issuer with blockchain address not found') {
-      this.displayOverlay = true;
-      return;
-    }
+    // if (this.response.data.error === 'Issuer with blockchain address not found') {
+    //   this.displayOverlay = true;
+    //   return;
+    // }
     if (this.categorySelected === 'artwork' || this.categorySelected === 'movieRight' || this.categorySelected === 'musicRight' || this.categorySelected === 'book' ) {
      } else {
       this.userActions.addSingle('error', 'Failed', 'Please make sure you select a category.');
@@ -289,7 +289,7 @@ export class CreateAssetsComponent implements OnInit {
                 this.router.navigateByUrl('/profile').then(() => {
                   window.location.reload();
                 });
-              } else {        
+              } else {
                 this.spinner.hide();
                 this.userActions.addSingle('error', 'Failed', 'There has been an error while trying to issue this asset, please try again.');
               }
