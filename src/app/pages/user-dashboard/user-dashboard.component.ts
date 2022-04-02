2acbcf53a09b98fe4da4749ed5c033631258c66b
Afolabi Opakunle <opakunleafolabi@gmail.com>
2022-04-02 10:48:41 +0100

    set for cloudinary

--- src/app/pages/user-dashboard/user-dashboard.component.ts
+++ src/app/pages/user-dashboard/user-dashboard.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {HotToastService} from '@ngneat/hot-toast';
 import { IArtwork } from 'src/app/core/components/slider/presentation.interface';
 import { MainService } from 'src/app/core/services/main.service';
 import { MetamaskService } from 'src/app/core/services/metamask.service';
+import {fileURLToPath} from 'url';
+import {ICreatorMedia} from '../../components/createArtwork.interface';
 import { IUser } from './user.interface';
 import { Clipboard } from '@angular/cdk/clipboard';
 import { IFollow, ILikes } from 'src/app/core/components/nftcard/event.interface';
@@ -43,9 +46,20 @@ export class UserDashboardComponent implements OnInit {
   coverImage: string = "/assets/img/default-cover.png";
   another: any [] = [];
   error: string;
+  showProfileUpload = false;
+  displayPicture;
+  previewMedia: any;
+  private errorMessage: string;
+  private image: boolean;
+  private previewArray:any = [];
+  public preview: any;
+  private mediaType: any;
+  private fileSize: number;
+  private media: Array<ICreatorMedia> = [];
 
   constructor(public mainService: MainService, public metamaskService: MetamaskService,
-    private clipboard: Clipboard, public userActions: UserActionsService, public auctionService: AuctionService) {
+    private clipboard: Clipboard, public userActions: UserActionsService, public auctionService: AuctionService,
+              public toast: HotToastService) {
     }
 
   ngOnInit(): void {
@@ -130,6 +144,49 @@ export class UserDashboardComponent implements OnInit {
   copyMessage(val){
     this.clipboard.copy(val);
     this.userActions.addSingle('global','success', 'Copied', 'Copied to clipboard!');
+  }
+
+  uploadPicture(form) {
+
+  }
+
+  clickedProfile() {
+    this.showProfileUpload = true;
+  }
+
+  check(event: any) {
+    const file = event.target.files[0]
+
+    this.errorMessage = '';
+    this.fileSize = file.size / 1024 / 1024;
+    if (this.fileSize > 10) {
+      this.errorMessage = 'Please Make sure that the file selected is not bigger than 10MB';
+      this.toast.error('Please Make sure that the file selected is not bigger than 10MB')
+      return;
+    } else {
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = this.handleFile.bind(this)
+      if ( /\.(jpe?g|gif|png)$/i.test(file.name) === false  ) {
+        this.errorMessage = 'Please select a file type of JPEG, GIF, PNG';
+        this.toast.error('Please select a file type of JPEG, GIF, PNG')
+        return;
+      } else {
+        if (/\.(jpe?g|gif|png)$/i.test(file.name) === true  ) {
+          this.image = true;
+          this.preview = file;
+          reader.onload = (event: any) => {
+            this.displayImage = event.target.result;
+            console.log(this.displayImage);
+          }
+        }
+      };
+    }
+  }
+
+  handleFile(event) {
+    const binaryString = event.target.result;
+    this.media.push(binaryString);
   }
 
 }
