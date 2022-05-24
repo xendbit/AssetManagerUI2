df76cc997c05bb576881818bcdcdea299b8dfa2a
spockula <chinedukogu@gmail.com>
2022-05-24 23:48:06 +0100

    update

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { IBlogGroup } from '../blog/blog.interfaces';
 import { IArtwork, IPresentation } from '../slider/presentation.interface';
 import { MainService } from '../../services/main.service';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-home',
@@ -13,54 +14,9 @@ export class HomeComponent implements OnInit {
   slide: IPresentation;
   blogs: IBlogGroup;
   artworks: IArtwork [];
-  another: any;
   categoryToIds: Map<string, Array<number> >
-  presentationData: IPresentation = {
-    "slides":  [
-      {
-        "id": "",
-        "category": "",
-        "tags": [],
-        "auctions": { "auctionId": "",
-        "cancelled": false, "chain": "", "currentBlock": "", "endBlock": "", "endDate": "", "finished": false, "highestBid": "",
-        "highestBidder": "", "id": 0, "minimumBid": "", "owner": "", "sellNowPrice": "", "sellNowTriggered": false,
-        "startBlock": "", "startDate": "", "started": true, "tokenId": ""},
-        "owner": {
-            "id": "",
-            "image": "",
-            "username": ""
-        },
-        "creator": {
-            "id": "",
-            "image": "",
-            "username": "",
-            "collections": [],
-            "type": ""
-        },
-        "featuredImage": {
-            "media": "/assets/img/slide1.jpeg",
-            "mediaType": 0
-        },
-        "isBidding": true,
-        "gallery": [],
-        "description": "Description",
-        "price": 0,
-        "currency": "ETH",
-        "likes": 0,
-        "type": "Artwork",
-        "lastAuctionId": 0,
-        "hasActiveAuction": true,
-        "symbol": "",
-        "tokenId": 0,
-        "name": "",
-        "dateIssued": new Date(),
-        "assetType": "digital",
-        "sold": false
-      },
-    ],
-  "presentationType": 1
-  };
-  
+  presentationData: any;
+
   transitions = '150ms cubic-bezier(0, 0, 0.2, 1)';
 
   responsiveOptions: any[] = [
@@ -77,21 +33,25 @@ export class HomeComponent implements OnInit {
       numVisible: 1
     }
   ];
-  constructor(public mainService: MainService, public auctionService: AuctionService) { }
+  constructor(public mainService: MainService,
+     public auctionService: AuctionService, 
+     private ngxService: NgxUiLoaderService) { }
 
   ngOnInit() {
+    this.ngxService.start();
     this.mainService.returnArtwork().subscribe((data: IArtwork []) => {
       if (data !== null) {
         this.artworks = data;
-        this.another = data
       }
     }, err => {
       console.log('artwork error =>', err)
+      this.ngxService.stop();
     });
     this.mainService.getPresentation().subscribe(async (res: any) => {
       this.slide = await res;
     }, err => {
       console.log('presentation error =>', err)
+      this.ngxService.stop();
     });
     this.mainService.getDrops().subscribe(async (res: any) => {
       if (res !== null) {
@@ -99,11 +59,14 @@ export class HomeComponent implements OnInit {
       }
     }, err => {
       console.log('Drops error => ', err);
+      this.ngxService.stop();
     })
     this.mainService.getBlogPost().subscribe((data: IBlogGroup) => {
       this.blogs = data;
+      this.ngxService.stop();
     }, err => {
-      console.log('blog error =>', err)
+      console.log('blog error =>', err);
+      this.ngxService.stop();
     });
   }
 
