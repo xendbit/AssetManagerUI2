cc928561da1d61b75da49804386460dd9cd304a4
spockula <chinedukogu@gmail.com>
2022-07-22 16:46:04 +0100

    update

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -11,9 +11,9 @@ headers = headers.append('api-key', apiKey);
 export const environment: AppEnvironment = {
   production: true,
   headers: headers,
-  baseApiUrl: 'https://lb.niftyrow.io/api/yasuke/',
-  extraUrl: 'https://lb.niftyrow.io/yasuke-server/',
-  icoUrl: 'https://lb.niftyrow.io/api/ico/whitelist/',
+  baseApiUrl: 'https://lb.niftyrow.net/api/yasuke/',
+  extraUrl: 'https://lb.niftyrow.net/yasuke-server/',
+  icoUrl: 'https://lb.niftyrow.net/api/ico/whitelist/',
   security: {
     allowedOrigins: 'https://niftyrow.io/'
   }
