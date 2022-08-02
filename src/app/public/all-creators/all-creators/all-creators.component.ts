85894760580e842d3d73d96a1debcfd256df8093
Afolabi Opakunle <opakunleafolabi@gmail.com>
2022-08-02 12:32:45 +0100

    all creators implemented

--- src/app/public/all-creators/all-creators/all-creators.component.ts
+++ src/app/public/all-creators/all-creators/all-creators.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import AllCreators from './all-creators.json'
 @Component({
   selector: 'app-all-creators',
   templateUrl: './all-creators.component.html',
   styleUrls: ['./all-creators.component.scss']
 })
 export class AllCreatorsComponent implements OnInit {
+
+  creators: any[] = AllCreators;
 
   constructor() { }
 
