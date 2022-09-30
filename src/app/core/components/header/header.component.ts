333fefffb77d87011e91988c6b1999e5da313c35
spockula <chinedukogu@gmail.com>
2022-09-30 09:06:24 +0100

    update to switch

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -30,6 +30,7 @@ export class HeaderComponent implements OnInit {
   darkMode$ = this.darkModeService.darkMode$;
   username: string = '';
   activeChain: string = ''; //
+  activeMarket: string = ''; //
   showChains: boolean = true;
   constructor(
     public mainService: MainService,
@@ -40,8 +41,9 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
     this.userWallet = localStorage.getItem('userWallet');
-    this.activeChain = localStorage.getItem('currentMarket');
-    if (this.activeChain === 'physical') {
+    this.activeChain = localStorage.getItem('currentChain');
+    this.activeMarket = localStorage.getItem('currentMarket');
+    if (this.activeMarket === 'physical') {
       this.showChains = false;
     }
     if (this.userWallet !== null) {
